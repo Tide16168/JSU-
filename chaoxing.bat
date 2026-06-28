df4d2b8eb9bbfd8d042756a68521b0e4b8101631
Holmes2718 <153147881+Holmes2718@users.noreply.github.com>
2026-06-28 23:59:25 +0800

    update docs

--- chaoxing.bat
+++ chaoxing.bat
@@ -2,4 +2,4 @@
 cd 你的\ChaoXingLibrarySeatReservation-main路径
 "改为你的python.exe路径" main.py >> run_log.txt 2>&1
 taskkill /f /im python.exe >nul 2>&1
-shutdown /s /t 0  # 我这里是自动关机了，需要什么自己修改
+shutdown /s /t 0   #  我这里是自动关机了，需要什么自己修改
